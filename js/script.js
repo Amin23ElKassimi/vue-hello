35239e2618f317682b3ff35d88a80b2b2ab26ecd
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-11-30 17:31:10 +0100

    To Jacopo

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Esercizio di oggi: Vue Hello
+// nome repo: vue-hello
+
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+// :githubparrot: :githubparrot: :githubparrot:
+
+
+
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        
+        return {
+            aminMessage: 'A Salam Aleikum'
+        }
+        
+    },
+}).mount('#title');
